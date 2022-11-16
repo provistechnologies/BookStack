d0490e6f745393fcb199f719780867b9f93eac8f
Ravi <ravindra.kumawat@provistechnologies.com>
2022-11-16 14:16:36 +0530

    Update code for tracker api to follow the project coding standard

--- app/Actions/UserSessionRepo.php
+++ app/Actions/UserSessionRepo.php
@@ -3,8 +3,6 @@
 namespace BookStack\Actions;
 
 use BookStack\Uploads\ScreenshotRepo;
-use Illuminate\Support\Facades\Auth;
-use Carbon\Carbon;
 
 /**
  * Class CommentRepo.
@@ -47,66 +45,29 @@ class UserSessionRepo
         return $screenshotData;
     }
 
-    public function checkSession($authUser_id, $current_date)
+    public function getSession($authUser_id, $current_date)
     {
         return $this->userSession->where('user_id', $authUser_id)->where('session_date', $current_date)->first();
     }
-    
-    // Apis functions for tracker
-    
-    public function startUserSession()
-    {
-        if (Auth::user()) {
-            $current_date = Carbon::now()->toDateString();
-            $authUser_id = Auth::user()->id;
-            $session = $this->userSession->where('user_id', $authUser_id)->where('session_date', $current_date)->first();
-            if (!empty($session)) {
-                return response()->json([
-                    'success' => true,
-                    'data' => $session,
-                ], 200);
 
-            } else {
-                $sessionData = [];
-                $sessionData['user_id'] = $authUser_id;
-                $sessionData['session_date'] =  $current_date;
-                $sessionData['session_start_time'] = Carbon::now();
-                $sessionDetail = $this->userSession->create($sessionData);
-                return response()->json([
-                    'success' => true,
-                    'data' => $sessionDetail,
-                ], 200);
-            }
-        }
+    public function createNewSession($sessionData)
+    {
+        return $this->userSession->create($sessionData);
     }
 
-    public function updateUserSession($request_data)
+    public function getSessionById($session_id)
     {
-        if (Auth::user()) {
-            $session = $this->userSession->where('id', $request_data->session_id)->first();
-            if ($request_data->session_end_time) {
-                $end_session = [
-                    'session_end_time' => $request_data->session_end_time,
-                ];
-                $session->fill($end_session)->save();
-            }
-            $screnshotData = [];
-            if (!empty($session)) {
-                $screnshotData['session_id'] = $session->id;
-                $screnshotData['screenshot_time'] = $request_data->screenshot_time;
-                $screnshotData['key_count'] = $request_data->key_count;
-                if (!empty($request_data->file('screenshot'))) {
-                    $imageUpload = $request_data->file('screenshot');
-                    $screenshotData = $this->screenshotRepo->saveScreemshot($imageUpload, $session->id);
-                    $screnshotData['screenshot'] = $screenshotData->url;
-                    $screnshotData = $this->screenshot->create($screnshotData);
-                }
-            }
-            return response()->json([
-                'success' => true,
-                'data' => $session,
-            ], 200);
-        }
+        return $this->userSession->where('id', $session_id)->first();
+    }
+
+    public function addScreenshot($screnshotData)
+    {
+        return $this->screenshot->create($screnshotData);
+    }
+
+    public function saveEndTime($session, $end_session)
+    {
+        $session->fill($end_session)->save();
     }
 
 }
--- app/Uploads/ScreenshotRepo.php
+++ app/Uploads/ScreenshotRepo.php
@@ -2,14 +2,13 @@
 
 namespace BookStack\Uploads;
 
-use Illuminate\Support\Str;
+use BookStack\Exceptions\FileUploadException;
 use Illuminate\Contracts\Filesystem\Filesystem as Storage;
 use Illuminate\Filesystem\FilesystemManager;
 
 use BookStack\Exceptions\ImageUploadException;
 use Exception;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
-use League\Flysystem\Util;
 use Illuminate\Support\Facades\Log;
 
 
@@ -27,148 +26,35 @@ class ScreenshotRepo
         $this->fileSystem = $fileSystem;
     }
 
+
     /**
      * Save a new image into storage and return the new image.
      *
      * @throws ImageUploadException
      */
-    public function saveScreemshot(UploadedFile $uploadFile, $session_id)
+    public function saveScreenshot(UploadedFile $uploadedFile, $session_id)
     {
-        $image = $this->saveNewFromUpload($uploadFile, $session_id);
-        return $image;
+        return $this->putFileInStorage($uploadedFile, $session_id);
     }
 
-    /**
-     * Saves a new image from an upload.
-     *
-     * @throws ImageUploadException
-     *
-     * @return mixed
-     */
-    public function saveNewFromUpload( UploadedFile $uploadedFile, $session_id) {
-        $imageName = $uploadedFile->getClientOriginalName();
-        $imageData = file_get_contents($uploadedFile->getRealPath());
-
-        return $this->saveNew($imageName, $imageData, $session_id);
-    }
-
-    /**
-     * Save a new image into storage.
-     *
-     * @throws ImageUploadException
-     */
-    public function saveNew(string $imageName, string $imageData, $session_id)
+    protected function putFileInStorage(UploadedFile $uploadedFile, $session_id): string
     {
-        $storage = $this->getStorageDisk($type ='');
-        $secureUploads = setting('app-secure-images');
-
-        $imagePath = '/uploads/screenshots/' . $session_id . '/';
-
-        while ($storage->exists($this->adjustPathForStorageDisk($imagePath . $imageName, $session_id))) {
-            $imageName = Str::random(3) . $imageName;
-        }
-
-        $fullPath = $imagePath . $imageName;
-        if ($secureUploads) {
-            $fullPath = $imagePath . Str::random(16) . '-' . $imageName;
-        }
-
+        $storage = $this->getStorageDisk();
+        $imagePath = '/uploads/screenshots/' . $session_id;
         try {
-            $this->saveImageDataInPublicSpace($storage, $this->adjustPathForStorageDisk($fullPath, $type), $imageData);
-        } catch (Exception $e) {
-            Log::error('Error when attempting image upload:' . $e->getMessage());
-
-            throw new ImageUploadException(trans('errors.path_not_writable', ['filePath' => $fullPath]));
-        }
-
-        $imageDetails = [
-            'name'        => $imageName,
-            'path'        => $fullPath,
-            'url'         => $this->getPublicUrl($fullPath),
-            'type'        => $type,
-            'uploaded_to' => 0,
-        ];
-
-        if (user()->id !== 0) {
-            $userId = user()->id;
-            $imageDetails['created_by'] = $userId;
-            $imageDetails['updated_by'] = $userId;
-        }
-
-        $image = (new Image())->forceFill($imageDetails);
-        $image->save();
-
-        return $image;
-    }
-
-    protected function adjustPathForStorageDisk(string $path, string $imageType = ''): string
-    {
-        $path = Util::normalizePath(str_replace('uploads/screenshots/', '', $path));
-
-        if ($this->usingSecureImages($imageType)) {
+            $path = $storage->put($imagePath, $uploadedFile);
             return $path;
-        }
-
-        return 'uploads/screenshots/' . $path;
-    }
-
-    protected function usingSecureImages(string $imageType = 'gallery'): bool
-    {
-        return $this->getStorageDiskName($imageType) === 'local_secure_images';
-    }
-
-    protected function getStorageDisk(string $imageType = ''): Storage
-    {
-        return $this->fileSystem->disk($this->getStorageDiskName($imageType));
-    }
-
-    protected function getStorageDiskName(string $imageType): string
-    {
-        $storageType = config('filesystems.images');
-
-        // Ensure system images (App logo) are uploaded to a public space
-        if ($imageType === 'system' && $storageType === 'local_secure') {
-            $storageType = 'local';
-        }
-
-        if ($storageType === 'local_secure' || $storageType === 'local_secure_restricted') {
-            $storageType = 'local_secure_images';
-        }
-
-        return $storageType;
-    }
-
-    protected function saveImageDataInPublicSpace(Storage $storage, string $path, string $data)
-    {
-        $storage->put($path, $data);
-
-        $usingS3 = strtolower(config('filesystems.images')) === 's3';
-        $usingS3Like = $usingS3 && !is_null(config('filesystems.disks.s3.endpoint'));
-        if (!$usingS3Like) {
-            $storage->setVisibility($path, 'public');
+        } catch (Exception $e) {
+            Log::error('Error when attempting file upload:' . $e->getMessage());
+            throw new FileUploadException(trans('errors.path_not_writable', ['filePath' => $imagePath]));
         }
     }
 
-    private function getPublicUrl(string $filePath): string
+    /**
+     * Get the storage that will be used for storing files.
+     */
+    protected function getStorageDisk(): Storage
     {
-        if (is_null($this->storageUrl)) {
-            $storageUrl = config('filesystems.url');
-
-            if ($storageUrl == false && config('filesystems.images') === 's3') {
-                $storageDetails = config('filesystems.disks.s3');
-                if (strpos($storageDetails['bucket'], '.') === false) {
-                    $storageUrl = 'https://' . $storageDetails['bucket'] . '.s3.amazonaws.com';
-                } else {
-                    $storageUrl = 'https://s3-' . $storageDetails['region'] . '.amazonaws.com/' . $storageDetails['bucket'];
-                }
-            }
-
-            $this->storageUrl = $storageUrl;
-        }
-
-        $basePath = ($this->storageUrl == false) ? url('/') : $this->storageUrl;
-
-        return rtrim($basePath, '/') . $filePath;
+        return $this->fileSystem->disk(config('filesystems.screenshots'));
     }
-
 }
